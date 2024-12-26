ab2175aa6b128792db4523d19ca9d1e099d8f7c7
Mahaboob2k21 <mahaboobbasha2k21@gmail.com>
2024-12-26 16:56:33 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import UserForm from './components/UserForm';
+import UserDashboard from './components/UserDashboard';
+
+function App() {
+    return (
+        <div>
+            <h1>User Management System</h1>
+            <UserForm />
+            <UserDashboard />
+        </div>
+    );
+}
+
+export default App;
+            
