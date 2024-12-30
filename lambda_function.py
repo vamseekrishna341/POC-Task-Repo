bc6fed2b843a9ccc6bf58440a54ee0f1dcee20f8
vamseekrishna341 <122243483+vamseekrishna341@users.noreply.github.com>
2024-12-30 15:38:29 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,8 @@
-import boto3
-
 def lambda_handler(event, context):
     """
-    Example Lambda function that fetches information about EC2 instances.
+    Example Lambda function that returns 'Hello World' when tested.
     """
-    ec2 = boto3.client('ec2')
-    response = ec2.describe_instances()
     return {
         'statusCode': 200,
-        'body': response
+        'body': 'Hello World'
     }
