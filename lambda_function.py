b5c8b9d5cd72fe232e37bbdb7c85f3b353028235
vamseekrishna341 <122243483+vamseekrishna341@users.noreply.github.com>
2025-01-02 13:06:25 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,5 @@
 def lambda_handler(event, context):
-    """
-    Example Lambda function that returns 'Hello World' when tested.
-    """
     return {
-        'statusCode': 200,
-        'body': 'Hello World'
+        "statusCode": 200,
+        "body": "Hello World"
     }
